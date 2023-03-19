09c1415447d93860840af48781f000b3993fea41
Zach Cygan <zachcygan@gmail.com>
2023-03-18 20:32:26 -0700

    working on seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+insert into department (name)
+values ('Management')
+       ('Engineering')
+       ('Accounting')
+       ('Warehouse')
+       ('Sales')
+
+insert into role (title, salary, department_id)
+values ('')
+
+
+insert into employee (first_name, last_name, role_id, manager_id)
+values ('Zachary', 'Cygan', )
