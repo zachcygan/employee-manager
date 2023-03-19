8a03bd5c6b1d895d028abb5050274146158e4148
Zachary Cygan <zachcygan@gmail.com>
2023-03-18 20:08:50 -0700

    added schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+drop database if exists employee_db;
+create database employee_db;
+
+use employee_db;
+
+create table department (
+    id int not null auto_increment primary key,
+    name VARCHAR(30)
+);
+
+create table role (
+    id int not null auto_increment primary key,
+    title VARCHAR(30),
+    salary decimal not null,
+    department_id int,
+    FOREIGN key (department_id)
+    REFERENCES department(id)
+    on delete set null
+);
+
+create table employee (
+    id int not null auto_increment primary key,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id int,
+    manager_id int,
+    foreign key (role_id)
+    references role(id)
+    on delete set null
+);
