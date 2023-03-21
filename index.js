53e2f1e53762c6d15cce6e5d331b33807a3df2c2
Zachary Cygan <zachcygan@gmail.com>
2023-03-20 17:05:44 -0700

    removed a commented section

--- index.js
+++ index.js
@@ -123,74 +123,7 @@ const init = async () => {
     console.clear();
 
 
-    // while (adding) {
-    //     let choice = await startOptions(startMessage);
-
-    //     if (choice.choice === 'view all departments') {
-    //         const [row, fields] = await db.query('select * from department')
-
-    //         console.log(fields.map(field => field.name).join('\t'));
-    //         console.log('-'.repeat(50));
-    //         // Print each row of the table
-    //         row.forEach(result => {
-    //             console.log(Object.values(result).join('\t'));
-
-    //         });
-
-    //         // console.log('Viewing all departments: \n');
-    //         // console.log(row)
-    //     }
-
-    //     if (choice.choice === 'view all roles') {
-    //         const [row, fields] = await db.promise().query('select * from role')
-
-    //         console.log('Viewing all departments: \n');
-    //         console.log(row)
-    //     }
-
-    //     if (choice.choice === 'quit') {
-    //         adding = false;
-    //         break;
-    //     }
-    // }
-
-        // switch (choice.choice) {
-        //     case 'view all departments':
-        //         db.query('select * from department', (err, results) => {
-        //             if (err) {
-        //                 console.log(err)
-        //             } else {
-        //                 const table = cTable.getTable(results)
-        //                 console.log(table);
-        //             }
-        //         });
-        //         console.log('Viewing all departments: \n');
-        //         break;
-        //     case 'view all roles':
-        //         console.log('view all roles');
-        //         break;
-        //     case 'view all employees':
-        //         console.log('view all employees');
-        //         break;
-        //     case 'add a department':
-        //         console.log('add a department');
-        //         break;
-        //     case 'add a role':
-        //         console.log('add a role');
-        //         break;
-        //     case 'add an employee':
-        //         console.log('add an employee');
-        //         break;
-        //     case 'update an employee role':
-        //         console.log('update an employee role');
-        //         break;
-        //     case 'quit':
-        //         adding = false;
-        //         break;
-        //     default:
-        //         adding = false;
-        //         break;
-        // }
+    
     }
 
 
